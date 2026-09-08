25be5fe227df48c8c47cf6873cd4329bc060ef04
Caio Farias <caio.farias@liferay.com>
2026-09-08 14:42:28 -0300

    LPD-93649 Unify the FIPS test utilities
    FIPSAlgorithmTestUtil and FIPSModeTestUtil both held FIPS test helpers, so they are merged into FIPSModeTestUtil in portal-test. The getChannelPropertiesLocation helper now takes the caller's class because DependenciesTestUtil resolves fixtures relative to the class it is given.

--- modules/apps/frontend-js/frontend-js-web/src/test/java/com/liferay/frontend/js/web/internal/hashed/files/HashedFilesRegistryImplTest.java
+++ modules/apps/frontend-js/frontend-js-web/src/test/java/com/liferay/frontend/js/web/internal/hashed/files/HashedFilesRegistryImplTest.java
@@ -10,7 +10,7 @@ import com.liferay.frontend.js.web.test.util.FrontendJSWebTestUtil;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.frontend.hashed.files.HashedFilesUtil;
 import com.liferay.portal.kernel.test.ReflectionTestUtil;
-import com.liferay.portal.kernel.test.util.FIPSAlgorithmTestUtil;
+import com.liferay.portal.kernel.test.util.FIPSModeTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.util.DigesterUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
@@ -93,7 +93,7 @@ public class HashedFilesRegistryImplTest {
 				"/o/frontend-js-web/main.css", RandomTestUtil.randomString())
 		).build();
 
-		FIPSAlgorithmTestUtil.assertAlgorithmSwitch(
+		FIPSModeTestUtil.assertAlgorithmSwitch(
 			DigesterUtil.MD5, MessageDigest.class, DigesterUtil.SHA_256,
 			MessageDigest::getInstance,
 			() -> ReflectionTestUtil.invoke(
